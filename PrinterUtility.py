ae621a57f554188882e66ceff75617b6821c122e
gopi2230 <gopi.psts@gmail.com>
2024-02-08 12:03:51 +0530

    fastapi implemented

--- PrinterUtility.py
+++ PrinterUtility.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+from fastapi import FastAPI,APIRouter,UploadFile,Form,Request,Response
+from fastapi.middleware.cors import CORSMiddleware
 from flask_cors import CORS
 import os
 import win32print
@@ -6,22 +8,50 @@ import win32api
 from urllib.parse import quote
 import uuid
 import time
+import asyncio
+import uvicorn
+from typing import List
+import subprocess
+
+app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-app = Flask(__name__)
-CORS(app, origins='*')
+# CORS(app, origins='*')
 
 
-@app.route('/')
-def home():
-    return 'Welcome to the Home Page!'
 
 
-@app.route('/process_form', methods=['POST'])
-def process_form():
+@app.get("/")
+def index():
+    return "Welcome to the Printer Utility"
 
-    pdf_file = request.files["file"]
-    default_printer_name = request.form.get('printer_name')
+
+@app.post('/process_form')
+async def process_form(file: UploadFile, printer_name: str = Form(...) ):   
+
+    default_printer_name = printer_name
+
+    user_home = os.path.expanduser("~")  
+    documents_folder = os.path.join(user_home, "Documents")
+    destination_folder = f'{documents_folder}//printedFiles/'
+
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
 
     printer_list = []
     printers = win32print.EnumPrinters(2)
@@ -32,52 +62,82 @@ def process_form():
     if default_printer_name in printer_list:
         print(f"{default_printer_name} is in the list.")
     else:
-        return jsonify({'message': 'An error occurred:', 'response': 'The selected printer does not exist on the device'}), 400
+        return {'message': 'An error occurred:', 'response': 'The selected printer does not exist on the device', 'status_code': 400  }
 
-    # Get the user's home directory
-    user_home = os.path.expanduser("~")
-
-    # Construct the path to the "Documents" folder
-    documents_folder = os.path.join(user_home, "Documents")
-    destination_folder = f'{documents_folder}//printedFiles/'
-
-    if not os.path.exists(destination_folder):
-        os.makedirs(destination_folder)
 
     unique_identifier = str(uuid.uuid4().hex)
     file_extension = ".pdf"
-    new_file_name = f"{
-        destination_folder}/new_file_{unique_identifier}{file_extension}"
+    new_file_name = f"{destination_folder}/new_file_{unique_identifier}{file_extension}"
 
-    pdf_file.save(new_file_name)
+    with open(new_file_name, "wb") as f:             
+            f.write(file.file.read())
 
     try:
         abs_file_path = os.path.abspath(f"{new_file_name}")
-        print_result = print_files(abs_file_path, default_printer_name)
+        print_result = await print_files(abs_file_path, default_printer_name)
 
-        response = {'message': 'File printed', 'response': print_result}
 
-        time.sleep(12)
+        # time.sleep(12)
         if os.path.exists(new_file_name):
             os.remove(new_file_name)
 
-        return jsonify(response), 200
-
+        response = {'message': 'File printed', 'response': print_result, 'status_code': 200 }
+        return response
+    
     except Exception as e:
         if os.path.exists(new_file_name):
             os.remove(new_file_name)
-        response = {'message': 'An error occurred:',
-                    'response': 'Something went wrong!'}
-        return jsonify(response), 400
+        response = {'message': 'An error occurred:', 'response': 'Something went wrong!', 'status_code': 400 }
+        return response
+    
 
 
-@app.route('/printer_list', methods=['GET'])
+
+@app.post('/print_multifile')
+async def printFile(files: List[UploadFile], printer_names: List[str]=Form(...)):  
+    
+    try:
+        user_home = os.path.expanduser("~")
+        documents_folder = os.path.join(user_home, "Documents")
+        destination_folder = os.path.join(documents_folder, "printedFiles")
+
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
+
+        for file in files:
+            for printer_name in printer_names:
+                printer_list = [printer[2] for printer in win32print.EnumPrinters(2)]
+
+                if printer_name not in printer_list:
+                    return {'message': 'An error occurred', 'response': f'The selected printer "{printer_name}" does not exist on the device', 'status_code': 400 }
+                
+                unique_identifier = str(uuid.uuid4().hex)         
+                file_path = f"{destination_folder}\\new_file_{unique_identifier}.pdf"
+                with open(file_path, "wb") as f:             
+                    f.write(file.file.read())
+
+                abs_file_path = os.path.abspath(file_path)
+
+                print_result = await print_files(abs_file_path, printer_name)
+                if os.path.exists(file_path):
+                    os.remove(file_path)    
+
+        response = {'message': 'File printed', 'response': print_result,'status_code':200 }
+        return response
+    
+    except Exception as e:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        response = {'message': 'An error occurred:','response': 'Something went wrong!','status_code':400}
+        return response
+
+
+@app.get('/printer_list')
 def printers_list():
 
     printer_list = []
-    # 2 means enumerate locally installed printers
     printers = win32print.EnumPrinters(2)
-    # print(printers)
+
 
     for printer in printers:
         printer_name = printer[2]
@@ -101,26 +161,19 @@ def printers_list():
     return printer_list
 
 
-def print_files(file_path, print_name):
+async def print_files(file_path, print_name):
+    try:      
 
-    # print_name = win32print.GetDefaultPrinter()
+        # print_name = win32print.GetDefaultPrinter()
+        # printer_handle = win32print.OpenPrinter(print_name)
+        command = f'PDFtoPrinter "{file_path}" "{print_name}"'      
+        
+        result = subprocess.run(command, shell=True, check=True)       
 
-    printer_handle = win32print.OpenPrinter(print_name)
-    command = f'PDFtoPrinter "{file_path}" "{print_name}"'
-
-    result = win32api.ShellExecute(
-        0, "open", "cmd.exe", f"/c {command}", None, 0)
-
-    # result=win32api.ShellExecute(
-    #     0,
-    #     "print",
-    #      file_path,
-    #     f'"{print_name}"',
-    #     ".",
-    #     0
-    # )
-    # return result
-
+            
+        return result
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
-    app.run(host='127.1.0.1', port=32568, debug=True)
+    uvicorn.run(app, port=32568, host='127.1.0.1')
