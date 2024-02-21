fdeab6cafa023b8d2d312ebcc95a74236c21405e
gopi2230 <gopi.psts@gmail.com>
2024-02-21 11:58:37 +0530

    multiple file print implemented

--- PrinterUtility.py
+++ PrinterUtility.py
@@ -6,7 +6,7 @@ import win32api
 from urllib.parse import quote
 import uuid
 import time
-
+import subprocess
 
 app = Flask(__name__)
 CORS(app, origins='*')
@@ -71,6 +71,53 @@ def process_form():
         return jsonify(response), 400
 
 
+@app.route('/print_multifile', methods=['POST'])
+def printFile():
+
+    pdf_file = request.files.getlist("files")
+    default_printer_name = request.form.getlist('printer_names')
+    print(pdf_file, 'pdf_file')
+    print(default_printer_name, 'default_printer_name')
+    try:
+        user_home = os.path.expanduser("~")
+        documents_folder = os.path.join(user_home, "Documents")
+        destination_folder = os.path.join(documents_folder, "printedFiles")
+
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
+
+        for file in pdf_file:
+            for printer_name in default_printer_name:
+                printer_list = [printer[2]
+                                for printer in win32print.EnumPrinters(2)]
+
+                if printer_name not in printer_list:
+                    return jsonify({'message': 'An error occurred', 'response': f'The selected printer "{printer_name}" does not exist on the device', 'status_code': 400}), 400
+
+                unique_identifier = str(uuid.uuid4().hex)
+                file_path = f"{destination_folder}\\new_file_{
+                    unique_identifier}.pdf"
+                with open(file_path, "wb") as f:
+                    f.write(file.file.read())
+
+                abs_file_path = os.path.abspath(file_path)
+
+                print_result = print_files(abs_file_path, printer_name)
+                if os.path.exists(file_path):
+                    os.remove(file_path)
+
+        response = {'message': 'File printed',
+                    'response': print_result, 'status_code': 200}
+        return jsonify(response), 200
+
+    except Exception as e:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        response = {'message': 'An error occurred:',
+                    'response': 'Something went wrong!', 'status_code': 400}
+        return jsonify(response), 400
+
+
 @app.route('/printer_list', methods=['GET'])
 def printers_list():
 
@@ -107,9 +154,10 @@ def print_files(file_path, print_name):
 
     printer_handle = win32print.OpenPrinter(print_name)
     command = f'PDFtoPrinter "{file_path}" "{print_name}"'
+    result = subprocess.run(command, shell=True, check=True)
 
-    result = win32api.ShellExecute(
-        0, "open", "cmd.exe", f"/c {command}", None, 0)
+    # result = win32api.ShellExecute(
+    #     0, "open", "cmd.exe", f"/c {command}", None, 0)
 
     # result=win32api.ShellExecute(
     #     0,
@@ -119,7 +167,7 @@ def print_files(file_path, print_name):
     #     ".",
     #     0
     # )
-    # return result
+    return result
 
 
 if __name__ == '__main__':
--- service.py
+++ service.py
@@ -1,34 +1,28 @@
+# service.py
+
 import win32serviceutil
 import win32service
 import win32event
 import servicemanager
 import socket
+import time
 import sys
-from flask import Flask
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-# Define more endpoints as needed
+from PrinterUtility import app
 
 
-class MyService(win32serviceutil.ServiceFramework):
-    _svc_name_ = 'MyService'  # The name of your service
-    _svc_display_name_ = 'Bsmart Printer Utility'  # The display name of your service
+class AppServerSvc(win32serviceutil.ServiceFramework):
+    _svc_name_ = 'FlaskService'
+    _svc_display_name_ = 'Printer Flask Service'
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
         self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
-        self.is_running = True
+        socket.setdefaulttimeout(60)
 
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
         win32event.SetEvent(self.hWaitStop)
-        self.is_running = False
 
     def SvcDoRun(self):
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
@@ -37,14 +31,13 @@ class MyService(win32serviceutil.ServiceFramework):
         self.main()
 
     def main(self):
-        # Start your Flask API here
-        app.run()
+        app.run(host='127.1.0.1', port=32568, debug=True)
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         servicemanager.Initialize()
-        servicemanager.PrepareToHostSingle(MyService)
+        servicemanager.PrepareToHostSingle(AppServerSvc)
         servicemanager.StartServiceCtrlDispatcher()
     else:
-        win32serviceutil.HandleCommandLine(MyService)
+        win32serviceutil.HandleCommandLine(AppServerSvc)
